38134ee2919d89899167187730c595cc89b52254
v.svistunov <v.svistunov@corp.nekki.ru>
2024-01-04 16:22:05 +0300

    сохранение фото на рабочий стол и в текстуру

--- lowEffortGallery/Assets/Scripts/Photocam/CaptureScreenshot.cs
+++ lowEffortGallery/Assets/Scripts/Photocam/CaptureScreenshot.cs
@@ -1,24 +1,19 @@
 using System;
 using UnityEngine;
 using System.IO;
-using Unity.VisualScripting;
 
 public class CaptureScreenshot : MonoBehaviour
 {
     public Camera cameraToCapture;
+    public GameObject photoObject;
     public string folderName = "galleryFiles";
     public string fileNamePrefix = "Screenshot";
 
     private int screenshotCount = 1;
+    private Texture2D savedTexture;
 
     private void Start()
     {
-        // Create the folder if it does not exist
-        string folderPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop), folderName);
-        if (!Directory.Exists(folderPath))
-        {
-            Directory.CreateDirectory(folderPath);
-        }
         cameraToCapture.gameObject.SetActive(false);
     }
 
@@ -29,32 +24,73 @@ public class CaptureScreenshot : MonoBehaviour
             Debug.Log("pressed");
             MakeScreenshot();
         }
-            
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (savedTexture != null)
+            {
+                ApplySavedTexture();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("saved");
+            SaveTextureToFile();
+        }
     }
 
     public void MakeScreenshot()
     {
         cameraToCapture.gameObject.SetActive(true);
-        // Set the file path
-        string filePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop), folderName, fileNamePrefix + screenshotCount.ToString() + ".png");
 
+        
         // Capture the screenshot
-        // Capture the screenshot from the selected camera
-        Texture2D screenshotTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
         cameraToCapture.targetTexture = renderTexture;
         cameraToCapture.Render();
         RenderTexture.active = renderTexture;
-        screenshotTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        savedTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        savedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        savedTexture.Apply();
         cameraToCapture.targetTexture = null;
         RenderTexture.active = null;
         Destroy(renderTexture);
-        byte[] bytes = screenshotTexture.EncodeToPNG();
-        File.WriteAllBytes(filePath, bytes);
-
+        
         // Increment the screenshot count
         screenshotCount++;
+
         cameraToCapture.gameObject.SetActive(false);
         Debug.Log("screenshot made");
+        
     }
+
+    private void ApplySavedTexture()
+    {
+        // Apply the saved texture to the photoObject
+        Renderer renderer = photoObject.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.mainTexture = savedTexture;
+            Debug.Log("Texture applied to photoObject");
+        }
+        else
+        {
+            Debug.LogError("photoObject does not have a Renderer component");
+        }
+    }
+
+    public void SaveTextureToFile()
+    {
+        // Create the folder if it does not exist
+        string folderPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop), folderName);
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+        //save texture to file
+        string filePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop), folderName, fileNamePrefix + screenshotCount.ToString() + ".png");
+        byte[] bytes = savedTexture.EncodeToPNG();
+        File.WriteAllBytes(filePath, bytes);
+    }
+    
 }
