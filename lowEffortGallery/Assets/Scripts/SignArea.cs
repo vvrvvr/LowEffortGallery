a594e825ea0c92de22b70e54d9296b2f9bd134cb
v.svistunov <v.svistunov@corp.nekki.ru>
2024-01-07 14:31:40 +0300

    Указатель движения

---
+++ lowEffortGallery/Assets/Scripts/SignArea.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignArea : MonoBehaviour
+{
+    public GameObject Sign;
+
+    private void Start()
+    {
+        Sign.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Sign.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Sign.SetActive(false);
+        }
+    }
+}
