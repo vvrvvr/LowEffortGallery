418f1803e8b9c789f7ac8d899cee57929511c136
v.svistunov <v.svistunov@corp.nekki.ru>
2024-01-29 16:19:51 +0300

    Выходы, экспликация

--- lowEffortGallery/Assets/Scripts/FollowExit.cs
+++ lowEffortGallery/Assets/Scripts/FollowExit.cs
@@ -2,30 +2,14 @@ using UnityEngine;
 
 public class FollowExit : MonoBehaviour
 {
-    public Transform anchorDefaultController;
-    private Transform anchor;
-    public Transform anchorFlyCam;
-
-    private void Start()
-    {
-        if (GameManager.Instance.isFlyCam)
-        {
-            anchor = anchorFlyCam;
-        }
-        else
-        {
-            anchor = anchorDefaultController;
-        }
-    }
-
+    public Transform anchorController;
+    
     void Update()
     {
-        if (anchor != null)
-        {
-            Vector3 anchorPosition = anchor.position;
+        Vector3 anchorPosition = anchorController.position;
             anchorPosition.y = transform.position.y;
             transform.position = anchorPosition;
-        }
+        
     }
     
 }
