59c36c228d4e5539fcbb77c3659f44f55323eb46
v.svistunov <v.svistunov@corp.nekki.ru>
2024-03-25 19:08:21 +0300

    разный питч всем криптидам

--- lowEffortGallery/Assets/Scripts/Cryptid/CryptidSound.cs
+++ lowEffortGallery/Assets/Scripts/Cryptid/CryptidSound.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class CryptidSound : MonoBehaviour
 {
-    
+    public float pitchMin = 0.6f;
+    public float pitchMax = 1f;
     private bool isOnce = true;
     private AudioSource _audio;
 
@@ -16,9 +17,10 @@ public class CryptidSound : MonoBehaviour
         if (other.CompareTag("Player") && isOnce)
         {
             Debug.Log("cryptid sound");
+            float pitch = Random.Range(pitchMin, pitchMax);
+            _audio.pitch = pitch;
             isOnce = false;
             _audio.Play();
-            
         }
     }
    
